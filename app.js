40058ebe1dec3eb09e990d298da4e10f59b5a169
Craig Loewen <crloewen@microsoft.com>
2021-10-05 23:53:23 -0700

    Updated prod push

--- app.js
+++ app.js
@@ -31,9 +31,6 @@ if (process.env.NODE_ENV == 'production') {
     hostPort = 3000;
 }
 
-console.log("Config: ");
-console.log(config);
-
 // Set up Mongoose connection
 const Schema = mongoose.Schema;
 const RepoInfo = new Schema({
