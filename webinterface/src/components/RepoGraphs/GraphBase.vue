64466f1f17e0c5c1ff20ce5bc4eabc4cbbc3370e
Mike Griese <migrie@microsoft.com>
2022-02-16 15:02:01 -0600

    set the min value to 0 for a milestone burndown

--- webinterface/src/components/RepoGraphs/GraphBase.vue
+++ webinterface/src/components/RepoGraphs/GraphBase.vue
@@ -20,16 +20,25 @@ export default {
           data: [40, 39, 10, 40, 39, 80, 40],
         },
       ],
+      options: {
+        // We always want responsive: true,maintainAspectRatio: false.
+        responsive: true,
+        maintainAspectRatio: false,
+        // If the user is looking at a milestone, we're gonna set the min value
+        // to 0, so the burndown is a bit clearer.
+        scales: null,
+      }
     };
   },
   methods: {
     startRender: function () {
+      console.log(this.options);
       this.renderChart(
         {
           labels: this.labels,
           datasets: this.datasets,
         },
-        { responsive: true, maintainAspectRatio: false }
+        this.options
       );
     },
     refreshData: function () {
@@ -44,11 +53,25 @@ export default {
             this.labels = graphData.labels;
             this.datasets = graphData.datasets;
 
+            // If the user is looking at a milestone, we're gonna set the min
+            // value to 0, so the burndown is a bit clearer. Otherwise, just
+            // clear that out.
+            if (this.inputQuery.milestones) {
+              this.options.scales = { "yAxes":[ { ticks: { "beginAtZero": true }} ] };
+            }
+            else{
+              this.options.scales = null;
+            }
+
             for (let i = 0; i < this.datasets.length; i++) {
               let datasetItem = this.datasets[i];
               datasetItem.backgroundColor = this.chartColors[i % this.chartColors.length];
-              // If there's a ton of data, the points on the graph only add noise
-              if (datasetItem.data.length > 50) { datasetItem.pointStyle='line'; /* datasetItem.tension=0; */ }
+
+              // If there's a ton of data, the points on the graph only add
+              // noise. Setting this to line will remove the points altogether.
+              if (datasetItem.data.length > 50) {
+                datasetItem.pointStyle='line';
+              }
             }
 
             this.loading = false;
