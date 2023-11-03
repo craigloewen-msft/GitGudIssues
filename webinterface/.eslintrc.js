4946a9cbd73a86be70561786762bbca8ec6f65a6
Craig Loewen <crloewen@microsoft.com>
2023-11-03 10:32:11 +0000

    Fixed NPM package versions

--- webinterface/.eslintrc.js
+++ webinterface/.eslintrc.js
@@ -8,11 +8,13 @@ module.exports = {
     'eslint:recommended'
   ],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: '@babel/eslint-parser'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-unused-vars': 'off'
+    'no-unused-vars': 'off',
+    'vue/multi-word-component-names': 0,
+    'vue/no-mutating-props': 0,
   }
 }
